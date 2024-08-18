614c2011cea2bc75633d60b988719e01042d8a9d
Alaafathalla <alaafathalla36@gmail.com>
2024-08-18 03:32:20 +0300

    first

--- index.js
+++ index.js
@@ -100,3 +100,19 @@ document.querySelectorAll('.faq-question').forEach(item => {
 });
 
 
+let mybutton = document.getElementById("scrollBtn");
+
+    window.onscroll = function () { scrollFunction() };
+
+    function scrollFunction() {
+      if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+      } else {
+        mybutton.style.display = "none";
+      }
+    }
+
+    function topFunction() {
+      document.body.scrollTop = 0;
+      document.documentElement.scrollTop = 0;
+    }
